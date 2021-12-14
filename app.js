2e96668398128e7d51040b5c4f3bd4b0971237c6
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-12-15 00:17:44 +0600

    add text copy part

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const pressKey = document.querySelector(".press_key h2");
-const keyBoardkeyCode = document.querySelector(".key_code h2");
-const keyBox = document.querySelector(".key_code ");
-const front = document.querySelector(".front ");
+const readKeyCode = document.querySelector(".key_code h2");
+const front = document.querySelector(".front");
+const clickCopy = document.querySelector("p");
 
 
 
 window.addEventListener("keydown",(e)=>{
     front.style.display = "none";
     pressKey.innerText = e.key; 
-    keyBoardkeyCode.innerText = e.keyCode;
+    readKeyCode.innerText = e.keyCode;
     
-    if(e.keyCode == 32){
+    if(e.keyCode === 32){
         pressKey.innerText = "Space" ;
     }
 });
 
+clickCopy.addEventListener("click",(e)=>{
+
+    const textBox = document.createElement("textarea");
+    textBox.setAttribute("readonly","");
+    textBox.style.position = "absolute";
+    textBox.value = readKeyCode.innerText;
+    document.body.appendChild(textBox);
+    // --- select & copy ---
+    textBox.select();
+    document.execCommand("copy");
+    document.body.removeChild(textBox);
+    
+    clickCopy.innerText = "Copied";
+    clickCopy.style.color = "#fff";
+    
+    setTimeout(()=>{
+        clickCopy.innerText = "Click To Copy";
+        clickCopy.style.color = "#000";
+    },2000)
+    
+})
 
 
 
